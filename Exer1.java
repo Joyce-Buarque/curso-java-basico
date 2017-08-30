7c980dd18b356c8bf870b839da77fa82c77b12c4
Joyce-Buarque <30418719+Joyce-Buarque@users.noreply.github.com>
2017-08-29 21:16:38 -0300

    Aula 13
    Exercícios aula 13 sobre condicionais.

---
+++ Exer1.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula13.exercicios;
+
+/** 
+ *  
+ *@author Joyce.silva
+ */
+
+public class Exer1 {
+
+	public static void main(String[] args) {
+
+		//Faça um programa que mostre na tela a msg "Alo mundo!";
+		
+		System.out.println("Alo mundo!");
+
+	}
+
+}
